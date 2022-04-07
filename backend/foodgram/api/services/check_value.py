1803d345f6be88e225c416e0180b8d02115341d8
Svetlana Logvinova <sunny7-7-7@yandex.ru>
2022-04-07 20:34:04 +0300

    filters edit

--- backend/foodgram/api/services/check_value.py
+++
@@ -1,9 +0,0 @@
-from decimal import Decimal
-
-
-def check_value_is_0_or_1(value: Decimal) -> bool:
-    """
-    Проверяет, что значение value равно либо 0, либо 1.
-    """
-
-    return value in (0, 1)
